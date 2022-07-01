81bc5386a7ccda7c80cd1f592ff2a25d2dd9f825
ZGGSONG <zggsong@foxmail.com>
2022-07-01 13:54:36 +0800

    init: 创建项目

---
+++ system/db.go
@@ -0,0 +1,33 @@
+package system
+
+import (
+	"coc-question-bank/model"
+	"errors"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"time"
+)
+
+const (
+	DBPATH = "./"
+)
+
+func InitDB() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(DBPATH), &gorm.Config{})
+	if err != nil {
+		return nil, errors.New("打开数据库失败: " + err.Error())
+	}
+
+	// 迁移 schema
+	err = db.AutoMigrate(&model.Subject{})
+	if err != nil {
+		return nil, errors.New("迁移数据库失败: " + err.Error())
+	}
+
+	//设置连接池
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxIdleConns(50)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Second * 30)
+	return db, nil
+}
