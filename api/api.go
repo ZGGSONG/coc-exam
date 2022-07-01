06cd0fdd21ff461f200a4158d6752f7108f38f2a
ZGGSONG <zggsong@foxmail.com>
2022-07-01 16:29:28 +0800

    perf: 修复输出

--- api/api.go
+++ api/api.go
@@ -6,19 +6,20 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func Ping(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"code":    http.StatusOK,
-		"message": "Pong",
+		"code": http.StatusOK,
+		"msg":  "Pong",
+		"data": "",
 	})
 }
 
 func Search(ctx *gin.Context) {
 	var model []model.Subject
 	var que = ctx.Query("question")
-	fmt.Println(que)
 	tmp := fmt.Sprintf("%v%v%v", "%", que, "%")
 	global.GLO_DB.Where("Question LIKE ?", tmp).Find(&model)
 	switch len(model) {
@@ -28,16 +29,33 @@ func Search(ctx *gin.Context) {
 			"msg":  "No Records Found...",
 			"data": "",
 		})
-	case 1:
+	case 1: //搜出一个答案
+		var answers string
+		mOptions := strings.Split(model[0].Options, " ")
+		for _, option := range mOptions {
+			switch model[0].Type {
+			case "multiple":
+				ans := strings.Split(model[0].Answer, "、")
+				for _, v := range ans {
+					if strings.Contains(option, v) {
+						answers += option + " "
+					}
+				}
+			default:
+				if strings.Contains(option, model[0].Answer) {
+					answers = option
+				}
+			}
+		}
 		ctx.JSON(http.StatusOK, gin.H{
 			"code": http.StatusOK,
-			"msg":  "Found One Record...",
+			"msg":  "Successfully",
 			"data": model[0],
 		})
 	default:
 		ctx.JSON(http.StatusOK, gin.H{
 			"code": http.StatusOK,
-			"msg":  "Found Multiple Records...",
+			"msg":  "Successfully",
 			"data": model,
 		})
 	}
